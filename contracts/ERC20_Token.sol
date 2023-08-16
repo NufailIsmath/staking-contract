26a3a3deb6c793e7354e82146de558ceedfd02fb
Nufail Ismath <36038687+NufailIsmath@users.noreply.github.com>
2023-08-16 09:12:57 +0530

    Staking Implementation

--- contracts/ERC20_Token.sol
+++ contracts/ERC20_Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
@@ -13,9 +13,13 @@ contract ERC20Token is
     ERC20Upgradeable,
     ERC20BurnableUpgradeable,
     PausableUpgradeable,
-    OwnableUpgradeable,
+    AccessControlUpgradeable,
     UUPSUpgradeable
 {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -25,24 +29,25 @@ contract ERC20Token is
         __ERC20_init("ERC20Token", "TKN");
         __ERC20Burnable_init();
         __Pausable_init();
-        __Ownable_init();
+        __AccessControl_init();
         __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
-    }
-
-    function burn(uint256 amount) public override onlyOwner {
-        super.burn(amount);
     }
 
     function _beforeTokenTransfer(
@@ -55,5 +60,5 @@ contract ERC20Token is
 
     function _authorizeUpgrade(
         address newImplementation
-    ) internal override onlyOwner {}
+    ) internal override onlyRole(UPGRADER_ROLE) {}
 }
--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -25,12 +25,30 @@ contract Staking is
 
     mapping(address => Staker) public stakers;
 
+    event Staked(address staker, uint256 amount, uint256 timestamp);
+    event Withdraw(address staker, uint256 amount, uint256 timestamp);
+
+    modifier onlyStaked() {
+        require(stakers[msg.sender].amount > 0, "Staking: No Staked Amount!");
+        _;
+    }
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
+    function initialize(
+        uint256 _stakingDuration,
+        uint256 _stakingStartTime,
+        bool _autoCompound,
+        address _token
+    ) public initializer {
+        stakingDuration = _stakingDuration;
+        stakingStartTime = _stakingStartTime;
+        autoCompound = _autoCompound;
+        token = IERC20Upgradeable(_token);
+
         __Pausable_init();
         __Ownable_init();
         __UUPSUpgradeable_init();
@@ -42,6 +60,72 @@ contract Staking is
 
     function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function stake(uint256 amount) public {
+        require(
+            block.timestamp > stakingStartTime,
+            "Staking: Staking start time has not met!"
+        );
+        require(amount > 0, "Staking: Amount must be greater than 0!");
+        require(stakers[msg.sender].amount == 0, "Staking: Already Staked!");
+        require(
+            token.balanceOf(msg.sender) >= amount,
+            "Staking: Insufficient Balance!"
+        );
+
+        token.transferFrom(msg.sender, address(this), amount);
+        stakers[msg.sender] = Staker(amount, block.timestamp);
+
+        emit Staked(msg.sender, amount, block.timestamp);
+    }
+
+    function withdrawStake() public onlyStaked {
+        Staker storage staker = stakers[msg.sender];
+        require(
+            block.timestamp >= staker.stakedTime + stakingDuration,
+            "Staking: Staking Period Not Over!"
+        );
+
+        uint256 withdrawAmount = staker.amount;
+        staker.amount = 0;
+        delete stakers[msg.sender];
+
+        token.transfer(msg.sender, withdrawAmount);
+        emit Withdraw(msg.sender, withdrawAmount, block.timestamp);
+    }
+
+    function viewStake() public view returns (Staker memory) {
+        return stakers[msg.sender];
+    }
+
+    function compoundStake() public onlyStaked {
+        Staker storage staker = stakers[msg.sender];
+        require(
+            block.timestamp >= staker.stakedTime + stakingDuration,
+            "Staking: Staking Period Not Over!"
+        );
+        require(autoCompound, "Staking: Auto Compound Not Enabled!");
+
+        uint256 reward = computeReward(msg.sender);
+        require(reward > 0, "Staking: No Reward To Compound!");
+
+        staker.stakedTime = block.timestamp;
+        token.mint(address(this), reward);
+        staker.amount += reward;
+
+        emit Staked(msg.sender, reward, staker.stakedTime);
+    }
+
+    function computeReward(
+        address stakerAddress
+    ) public view returns (uint256) {
+        Staker storage staker = stakers[stakerAddress];
+
+        uint256 elapsed = block.timestamp - staker.stakedTime;
+        uint256 rewards = (staker.amount * elapsed) / stakingDuration;
+
+        return rewards;
     }
 
     function _authorizeUpgrade(
